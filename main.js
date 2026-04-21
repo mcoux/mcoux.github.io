17bc83434c27e7e58373bc5098a551730feb9b40
Maxime coux <100847666+mcoux@users.noreply.github.com>
2026-04-21 18:07:20 +0200

    debut tabs

--- main.js
+++ main.js
@@ -151,3 +151,12 @@ function animate(){
 }
 
 animate()
+
+
+//Tabs display
+function openTab(ebt, tabName){
+    var contents = document.getElementsByClassName("tab");
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = "none";
+      }
+}
